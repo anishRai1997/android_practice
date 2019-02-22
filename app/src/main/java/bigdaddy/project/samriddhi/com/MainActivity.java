d55b92983d86b3dc658e72103ca2cc5d4dc29913
Anish <Anishrai1997@gmail.com>
2019-02-22 23:43:53 +0545

    My first android project yayy

---
+++ app/src/main/java/bigdaddy/project/samriddhi/com/MainActivity.java
@@ -0,0 +1,20 @@
+package bigdaddy.project.samriddhi.com;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "start", Toast.LENGTH_SHORT).show();
+    }
+}
